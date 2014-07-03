5633241bd27f2569317621e8d66ca1219f1a89c1
amo <amo@amodhas-imac>
2014-07-03 23:46:54 +1000

    adding test scanner class

---
+++ AmoTest/src/TestScanner.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class TestScanner {
+
+	 
+	  
+	 public static void main(String [] args) {
+		 
+		 printMessage();
+		 
+		 String name = inputName();
+		 
+		 System.out.println(name);
+		 
+	}
+
+
+	private static String inputName() {
+		// TODO Auto-generated method stub
+		 Scanner in = new Scanner(System.in);
+		 String name1 = in.next(); 
+		 return name1;
+	}
+
+
+	private static void printMessage() {
+		// TODO Auto-generated method stub
+		System.out.println ("Enter your name");
+	}
+}
